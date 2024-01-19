0bd899c732e893b2d72d88bef46547fd60f7727f
SulienP <112190974+SulienP@users.noreply.github.com>
2024-01-19 18:46:29 +0100

    :alien: |start connection with backend

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,26 @@
 // COMPONENTS
+import  { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from "../../components/Header/Header";
 import HeroCard from "../../components/HeroCard/HeroCard";
+import "./Home.css";
 
 // STYLES
 import "./Home.css";
 const Home = () => {
+    const [apiData, setApiData] = useState([]);
 
+  useEffect(() => {
+  
+
+    axios.get('http://localhost:8000/api/superHero')
+      .then(response => {
+        setApiData(response.data);
+      })
+      .catch(error => {
+        console.error('Erreur lors de la requête GET', error);
+      });
+  }, []);
   return (
     <>
       <Header/>
@@ -17,20 +32,21 @@ const Home = () => {
           </div>
         </div>
         <div className="home-cards">
-          <HeroCard />
-          <HeroCard />
-          <HeroCard />
-          <HeroCard />
-          <HeroCard />
-          <HeroCard />
-          <HeroCard />
-          <HeroCard />
-          <HeroCard />
-          <HeroCard />
-          <HeroCard />
-          <HeroCard />
+             {apiData.map(hero => (
+          <HeroCard
+                 key={hero.id_hero}
+                 firstname={hero.firstname}
+                 lastname={hero.lastname}
+                 description={hero.description}
+                 hairColor={hero.hair_color}
+                 groups={hero.group_names}
+                 origin={hero.origin_planet}
+          />
+        ))}
+        
         </div>
       </div>
+
     </>
   );
 };
